43babd7fd3304c18b3964f703b60d41ca63bd94a
chanhhiep <19130067@st.hcmuaf.edu.vn>
2023-04-18 22:51:54 +0700

    admin interfaces

---
+++ src/main/webapp/WEB-INF/views/admin/js/hidden.js
@@ -0,0 +1,12 @@
+function clickEditToggle(){   
+    var popup = document.getElementById("product_edit");
+    popup.classList.toggle('active');
+    var blur = document.getElementById("blur-action");
+    blur.classList.toggle('active');
+}
+function clickCreateToggle(){
+    var popup = document.getElementById("create-product");
+        popup.classList.toggle('active');
+    var blur = document.getElementById("blur-action");
+        blur.classList.toggle('active');
+}
