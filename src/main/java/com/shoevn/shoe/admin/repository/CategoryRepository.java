b8c58aa8b17aa972f91c1c999bbeb1e6a96712a6
chanhhiep <19130067@st.hcmuaf.edu.vn>
2023-04-30 03:49:12 +0700

    listing product

--- src/main/java/com/shoevn/shoe/admin/repository/CategoryRepository.java
+++ src/main/java/com/shoevn/shoe/admin/repository/CategoryRepository.java
@@ -4,5 +4,5 @@ import com.shoevn.shoe.Beans.Account;
 import com.shoevn.shoe.Beans.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category,Integer> {
+public interface CategoryRepository extends JpaRepository<Category,Long> {
 }
